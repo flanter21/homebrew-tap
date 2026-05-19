8739a56cd0c62e59ea51e0dd25fdc1db1d1d9677
flanter21 <flanter21@gmail.com>
2026-05-19 07:21:21 +0100

    fixed some versioning issues

--- Formula/ctrtool.rb
+++ Formula/ctrtool.rb
@@ -1,7 +1,7 @@
 class Ctrtool < Formula
   desc "General purpose reading/extraction tool for Nintendo 3DS file formats"
   homepage "https://github.com/3DSGuy/Project_CTR"
-  url "https://github.com/3DSGuy/Project_CTR/archive/refs/tags/ctrtool-v#{version}.tar.gz"
+  url "https://github.com/3DSGuy/Project_CTR/archive/refs/tags/ctrtool-v1.3.0.tar.gz"
   version "1.3.0"
   sha256 "6c0314928dea722f769cfa7257a963df0d7503962b2998a8c91ec6879fb86075"
   license "MIT"
